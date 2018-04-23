35c15c7a8f97838291553e784b0d1b377a7fa48b
yohe88 <31714273+yohe88@users.noreply.github.com>
2018-04-23 22:06:38 +0800

    Update and rename web/config.php to  web/config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,10 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAT6dJBGQ6ll8g8Fqvd0hZNME6B8rxE7b4');//这里改成您的api key
-define('GJ_CODE', 'HK');//国家代码
-define('SITE_NAME', 'YoHe');//网站名字
-define('TITLENAME', 'YouTube镜像');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'zyh15016688401@gmail.com');//邮箱
-?>
+<？PHP
+define（' ROOT_PART '，Root_part（））;
+define（' APIKEY '，' ********************************* '）; //这里改成你的api键
+define（' GJ_CODE '，' HK '）; //国家代码
+define（' SITE_NAME '，' YoHe '）; //网站名字
+define（' TITLENAME '，' Youtube镜像 '）; //网站名字
+define（' EN2DEKEY '，' AnQUP1XDEF435e67CtzDSh4 '）; //一个随机字符串，加解密KEY
+define（' EMAIL '，' 123@123.com '）; //邮箱
+？>
+  
